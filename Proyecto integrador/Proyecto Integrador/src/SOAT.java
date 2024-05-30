bad6291068ae62e8cc499eafcc0ed8081e77f1da
Jose <160917285+josemm12@users.noreply.github.com>
2024-05-30 16:46:29 -0500

    Add files via upload

---
+++ Proyecto integrador/Proyecto Integrador/src/SOAT.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class SOAT {
+    private JTable table1;
+    private JTable table2;
+}
---
+++ Proyecto integrador/Proyecto Integrador/src/Tecnomecanica.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Tecnomecanica {
+    private JTable table1;
+    private JTable table2;
+}
